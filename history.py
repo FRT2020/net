0f74a3e4efc48a3975e209f13e063f9d4a7692b1
FRT2020 <74301271+FRT2020@users.noreply.github.com>
2020-11-11 14:48:53 +0300

    Add files via upload

---
+++ history.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Wed Nov 11 11:55:47 2020)---
+runfile('C:/Users/Иван/.spyder-py3/temp.py', wdir='C:/Users/Иван/.spyder-py3')
+
+## ---(Wed Nov 11 12:05:38 2020)---
+runfile('C:/Users/Иван/.spyder-py3/temp.py', wdir='C:/Users/Иван/.spyder-py3')
+
+## ---(Wed Nov 11 12:12:42 2020)---
+runfile('C:/Users/Иван/.spyder-py3/temp.py', wdir='C:/Users/Иван/.spyder-py3')
+
+## ---(Wed Nov 11 12:27:00 2020)---
+runfile('C:/Users/Иван/.spyder-py3/temp.py', wdir='C:/Users/Иван/.spyder-py3')
+
+## ---(Wed Nov 11 12:31:48 2020)---
+runfile('C:/Users/Иван/.spyder-py3/temp.py', wdir='C:/Users/Иван/.spyder-py3')
+
+## ---(Wed Nov 11 12:44:12 2020)---
+runfile('C:/Users/Иван/.spyder-py3/temp.py', wdir='C:/Users/Иван/.spyder-py3')
---
+++ temp.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation, BatchNormalization, AveragePooling2D
+from tensorflow.keras.optimizers import SGD, RMSprop, Adam
+import tensorflow_datasets as tfds  # pip install tensorflow-datasets
+import tensorflow as tf
+import logging
+import numpy as np
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+tf.get_logger().setLevel(logging.ERROR)
+
+model = Sequential()
+model.add(Dense(2, input_dim=2, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.1))
+print(model.summary())
+
+
+
+def mnist_make_model2(image_w: int, image_h: int):
+   # Neural network model
+   model = Sequential()
+   model.add(Dense(1024, activation='relu', input_shape=(image_w*image_h,)))
+   model.add(Dropout(0.2))  # rate 0.2 - set 20% of inputs to zero
+   model.add(Dense(1024, activation='relu'))
+   model.add(Dropout(0.2))
+
+   model.add(Dense(10, activation='softmax'))
+
+   model.compile(loss='categorical_crossentropy', optimizer=RMSprop(),
+                             metrics=['accuracy'])
+   return model
+
+
+def mnist_mlp_train(model):
+   (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+   # x_train: 60000x28x28 array, x_test: 10000x28x28 array
+   image_size = x_train.shape[1]
+   train_data = x_train.reshape(x_train.shape[0], image_size*image_size)
+   test_data = x_test.reshape(x_test.shape[0], image_size*image_size)
+   train_data = train_data.astype('float32')
+   test_data = test_data.astype('float32')
+   train_data /= 255.0
+   test_data /= 255.0
+
+   # encode the labels - we have 10 output classes
+   # 3 -> [0 0 0 1 0 0 0 0 0 0], 5 -> [0 0 0 0 0 1 0 0 0 0]
+   num_classes = 10
+   train_labels_cat = keras.utils.to_categorical(y_train, num_classes)
+   test_labels_cat = keras.utils.to_categorical(y_test, num_classes)
+
+   print("Training the network...")
+   
+
+   # Start training the network
+   model.fit(train_data, train_labels_cat, epochs=8, batch_size=64, 
+verbose=1, validation_data=(test_data, test_labels_cat))
+
+model = mnist_make_model2(image_w=28, image_h=28)
+mnist_mlp_train(model)
+model.save('mlp_digits_28x28.h5')
+
+def mlp_digits_predict(model, image_file):
+   image_size = 28
+   img = keras.preprocessing.image.load_img(image_file, target_size=(image_size, image_size), color_mode='grayscale')
+   img_arr = np.expand_dims(img, axis=0)
+   img_arr = 1 - img_arr/255.0
+   img_arr = img_arr.reshape((1, image_size*image_size))
+
+   result = model.predict_classes([img_arr])
+   return result[0]
+
+model = tf.keras.models.load_model('mlp_digits_28x28.h5')
+print(mlp_digits_predict(model, 'digit_0.png'))
+print(mlp_digits_predict(model, 'digit_1.png'))
+print(mlp_digits_predict(model, 'digit_2.png'))
+print(mlp_digits_predict(model, 'digit_3.png'))
+print(mlp_digits_predict(model, 'digit_9.png'))
+
